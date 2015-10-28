610620aff04f6295363685102b4be8c2720eeaf3
Alejadro Villegas <alejandrov@kometsales.com>
2015-10-28 17:13:17 -0500

    Second Exercises
    Example2

---
+++ 2/views/index.jsx
@@ -0,0 +1,35 @@
+    var React = require('react');
+    
+    var TodoBox = React.createClass({
+      render: function() {
+        return (
+          <div className="todoBox">
+            <h1>Todos</h1>
+            <TodoList />
+            <TodoForm />
+          </div>
+        );
+      }
+    });
+    
+    var TodoList = React.createClass({
+      // Write code here
+      render: function {
+        return (<div class = "todoList">
+          I am a TodoList.
+        </div>)
+      }
+    });
+    
+    var TodoForm = React.createClass({
+      // Write code here
+      render: function{
+        return(
+          <div class = "todoForm">
+            I am a TodoForm.
+          </div>
+          )
+      }
+    });
+    
+    module.exports = TodoBox;
